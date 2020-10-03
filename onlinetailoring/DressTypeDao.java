7a45574f2f780d963dc1c13ef0aa94e373483d41
ChiragJindal1231998 <72062591+ChiragJindal1231998@users.noreply.github.com>
2020-10-03 17:14:18 +0530

    Update DressTypeDao.java

--- onlinetailoring/DressTypeDao.java
+++ onlinetailoring/DressTypeDao.java
@@ -1,7 +1,10 @@
 package tailoring;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class DressTypeDao {private JdbcTemplate jdbcTemplate;
+public class DressTypeDao {
+	private JdbcTemplate jdbcTemplate;
 
 public JdbcTemplate getJdbcTemplte() {
 	return jdbcTemplate;
@@ -12,11 +15,27 @@ public void setJdbcTemplte(JdbcTemplate jdbcTempalte) {
 }
 
 public int createDressType(DressType dressType) {
-	String sql = "insert into DressCategoryMaster values(?,?,?,?,?)";
+	String sql = "insert into DressTypeMaster values(?,?,?,?,?)";
 	int result = jdbcTemplate.update(sql, dressType.getDressTypeId(),dressType.getDressType(),dressType.getDressCategory(),dressType.getDressKind(),dressType.getPattern());
 	return result;
 
 }
+public int updateDressType(DressType dressType) {
+	String sql="update DressTypeMaster set dressType= ? ,dressCategory=?,dressKind=?,pattern=? where dressTypeId=?";
+	int result = jdbcTemplate.update(sql,dressType.getDressType(),dressType.getDressCategory(),dressType.getDressKind(),dressType.getPattern(),dressType.getDressTypeId());
+	return result ;
+	
+}
+public List <DressType> readDressType() {
+	String sql= "select* from DressTypeMaster";
+	List<DressType>dressType=jdbcTemplate.query(sql, new DressTypeRowMapper());
+	return dressType;
+	
+}
+public int deleteDressType(int dressTypeId) {
+	String sql="delete from DressTypeMaster where dressTypeId=?";
+	int result = jdbcTemplate.update(sql,dressTypeId);
+	return result ;
 
 
 }
