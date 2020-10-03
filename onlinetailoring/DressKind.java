b1af62143004658b51517f42caddb65d58b1a106
ChiragJindal1231998 <72062591+ChiragJindal1231998@users.noreply.github.com>
2020-10-03 11:47:35 +0530

    Add files via upload

---
+++ onlinetailoring/DressKind.java
@@ -0,0 +1,17 @@
+
+public class DressKind {
+	private int dressKindId ;
+	private String dressKind ;
+	public int getDressKindId() {
+		return dressKindId;
+	}
+	public void setDressKindId(int dressKindId) {
+		this.dressKindId = dressKindId;
+	}
+	public String getDressKind() {
+		return dressKind;
+	}
+	public void setDressKind(String dressKind) {
+		this.dressKind = dressKind;
+	}
+}
---
+++ onlinetailoring/DressType.java
@@ -0,0 +1,39 @@
+
+public class DressType {
+	private int dressTypeId;
+	private String dressType ;
+	private String dressCategory;
+	private String dressKind;
+	private String pattern ;
+	public int getDressTypeId() {
+		return dressTypeId;
+	}
+	public void setDressTypeId(int dressTypeId) {
+		this.dressTypeId = dressTypeId;
+	}
+	public String getDressType() {
+		return dressType;
+	}
+	public void setDressType(String dressType) {
+		this.dressType = dressType;
+	}
+	public String getDressCategory() {
+		return dressCategory;
+	}
+	public void setDressCategory(String dressCategory) {
+		this.dressCategory = dressCategory;
+	}
+	public String getDressKind() {
+		return dressKind;
+	}
+	public void setDressKind(String dressKind) {
+		this.dressKind = dressKind;
+	}
+	public String getPattern() {
+		return pattern;
+	}
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
+	
+}
