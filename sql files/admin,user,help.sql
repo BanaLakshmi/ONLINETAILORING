e9e94c02353323fe1200f03cd3ac202b6fab206c
BanaLakshmi <72138645+BanaLakshmi@users.noreply.github.com>
2020-10-17 09:02:36 +0530

    Update admin,user,help.sql

--- sql files/admin,user,help.sql
+++ sql files/admin,user,help.sql
@@ -2,6 +2,7 @@ Create database onlineTailoringStore;
 use onlineTailoringStore;
 create table UserMaster
 ( userId varchar(10) primary key,
+ username varchar(20),
 password varchar(10),
 firstName varchar(20),
 lastName varchar(20),
@@ -13,10 +14,14 @@ userCategory varchar(10),
 secretQuestions varchar(1000)
 );
 
-select*from AdminMaster;
+create table AdminMaster
+(adminId varchar(10) primary key,
+ password varchar(10)
+);
+
 create table Help
 (
-requestId varchar(10),
+requestId varchar(10) primary key,
 issue varchar(100),
 description varchar(1000),
 dateOfTicket Date
