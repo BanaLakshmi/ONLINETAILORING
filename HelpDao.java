2e7f3c9cac40b7c6f2e385f11422e449e09f7635
BanaLakshmi <72138645+BanaLakshmi@users.noreply.github.com>
2020-10-17 15:56:29 +0530

    Update HelpDao.java

--- HelpDao.java
+++ HelpDao.java
@@ -1,46 +1,48 @@
 package com.mfrp.onlinetailoring;
-
-import java.util.Date;
 import java.util.List;
+import java.util.ArrayList;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
+
 
 public class HelpDao {
-	//String requestId;
-	//String issue;
-	//String description;
-	//Date dateOfTicket;
-	private JdbcTemplate jdbcTemplate;
+	
+	HibernateTemplate ht;  
+
+public HibernateTemplate getHt() {
+		return ht;
+	}
+	public void setHt(HibernateTemplate ht) {
+		this.ht = ht;
+	}
+
+
+	
+	//method to save Help
+	public void saveHelp(Help help){  
+		ht.save(help);  
+	}  
+
+//method to update Help
+	public void updateHelp(Help help){  
+		ht.update(admin);  
+	} 
+//method to delete Help
+	public void deleteHelp(Help help){  
+		ht.delete(help);  
+	}   
+//method to return Help of given requestId 
+	public Help getByAdminId(int requestId){  
+		Help help=(Help)ht.get(Help.class,requestId);  
+		return help; 
+}
+} 
+
+
 	
 
-	public JdbcTemplate getJdbcTemplte() {
-		return jdbcTemplate;
-	}
+	
+	
+	
+	
 
-	public void setJdbcTemplte(JdbcTemplate jdbcTempalte) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-
-	public int createHelp(Help help) {
-		String sql = "insert into Help values(?,?,?)";
-		int result = jdbcTemplate.update(sql, help.getIssue(),help.getDescription(),help.getDateOfTicket());
-		return result;
-
-	}
-	public int updateHelp(Help help) {
-		String sql="update Help set issue=?,description=?,dateOfTicket=? where requestId=?";
-		int result = jdbcTemplate.update(sql,help.getIssue(),help.getDescription(),help.getDateOfTicket(),help.getRequestId());
-		return result ;
-		
-	}
-	public List <Help> readHelp() {
-		String sql= "select* from Help";
-		List<Help>help=jdbcTemplate.query(sql, new HelpRowMapper());
-		return help;
-		
-	}
-	public int deleteHelp(int requestId) {
-		String sql="delete from AdminMaster where adminId=?";
-		int result = jdbcTemplate.update(sql,requestId);
-		return result ;
-
-}
-}
